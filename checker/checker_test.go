3e21899295dec88682c32cd0564d0a028de1ee2e
amnez3a <maksimalhimov549@gmail.com>
2026-06-21 08:02:56 +0300

    test: add tests

---
+++ checker/checker_test.go
@@ -0,0 +1,32 @@
+package checker
+
+import (
+	"go-monitor/server"
+	"testing"
+)
+
+type MockCheckher struct {
+	Result string
+}
+
+func (m MockCheckher) Check(s server.Server) string {
+	return m.Result
+}
+
+func TestOnlineServer(t *testing.T) {
+	mock := MockCheckher{Result: "[✓] Alpine online"}
+	result := mock.Check(server.Server{Name: "Alpine"})
+
+	if result != "[✓] Alpine online" {
+		t.Errorf("got %s, want [✓] Alpine online", result)
+	}
+}
+
+func TestOffline(t *testing.T) {
+	mock := MockCheckher{Result: "[✗] Alpine offline"}
+	result := mock.Check(server.Server{Name: "Alpine"})
+
+	if result != "[✗] Alpine offline" {
+		t.Errorf("got %s, want [✗] Alpine offline", result)
+	}
+}
